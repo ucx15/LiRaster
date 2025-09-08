92ada9cc3a6f1a5cf40c2a338c40ab2efa234f43
ucx15 <83822686+ucx15@users.noreply.github.com>
2025-09-09 04:56:50 +0530

    Refactor build script to use Intermediate directory and remove Asm references

--- build.ps1
+++ build.ps1
@@ -8,25 +8,27 @@
 # -------- BUILD SETTINGS --------
 $C_FLAGS = "-Wall", "-Wextra", "-pedantic", "-std=c++20", "-masm=intel"
 
-$Optimization_flags = "-O3", "-mavx512f", "-march=native"
+$Optimization_flags = "-march=native"
+# $Optimization_flags = "-O3", "-mavx512f", "-march=native"
 # $Optimization_flags = "-ggdb", "-g3"
 
 $LINKER_FLAGS = "-lmingw32", "-lSDL2main", "-lSDL2", "-s"
 
 
-$buildAll = $true
+$buildAll = $false
 
 $out_file = "LiRaster.exe"
 
 $src_files = "main", "engine", "utils", "surface"
 
+$intermediate_dir = "Intermediate/"
 
+# Include Directories
 $include_dir = "Src/Include/"
-
 $sdl_inc_dir = "Libs/SDL2/include/SDL2"
 $stb_inc_dir = "Libs/"
 
-
+# Library Directories
 $sdl_lib_dir = "Libs/SDL2/lib"
 
 
@@ -37,8 +39,8 @@ if (!(Test-Path("./Out"))) {
 	mkdir Out
 }
 
-if (!(Test-Path("./Asm"))) {
-	mkdir Asm
+if (!(Test-Path($intermediate_dir))) {
+	mkdir $intermediate_dir
 }
 
 # removing previous build
@@ -52,21 +54,35 @@ Write-Output "Building:"
 foreach ($file in $src_files) {
 	if ($file) {
 
-		$modifyCondition = $true
-		if (Test-Path "Asm/${file}.s") {
+		$modifyCondition = $false
+
+		if (Test-Path "$intermediate_dir/${file}.o") {
+			$targetModifiedDate = (Get-Item "$intermediate_dir/${file}.o").LastWriteTime
+
+			# C++ Impl.
 			$sourceModifiedDate = (Get-Item "Src/${file}.cpp").LastWriteTime
-			$targetModifiedDate = (Get-Item "Asm/${file}.s").LastWriteTime
-			$modifyCondition = ($sourceModifiedDate -gt $targetModifiedDate)
+
+			# Header if exists
+			if (Test-Path "Src/Include/${file}.hpp") {
+				$headerModifiedDate = (Get-Item "Src/Include/${file}.hpp").LastWriteTime
+				$modifyCondition = $headerModifiedDate -gt $targetModifiedDate
+			}
+
+			$modifyCondition = $modifyCondition -or ($sourceModifiedDate -gt $targetModifiedDate)
+		}
+
+		else {
+			modifyCondition = $true
 		}
 
 		if (($modifyCondition -eq $true) -or ($buildAll -eq $true)) {
 			Write-Output "    ${file}.cpp"
-		
-			if (Test-Path "Asm/${file}.s") {
-				Remove-Item Asm/${file}.s
+
+			if (Test-Path "$intermediate_dir/${file}.o") {
+				Remove-Item "$intermediate_dir/${file}.o"
 			}
 
-			g++ $C_FLAGS -I $include_dir -I $stb_inc_dir -I $sdl_inc_dir -L $sdl_lib_dir $sdl_linkables -o Asm/${file}.s -S Src/${file}.cpp
+			g++ $C_FLAGS -I $include_dir -I $stb_inc_dir -I $sdl_inc_dir -o "$intermediate_dir/${file}.o" -c "Src/${file}.cpp"
 
 		}
 	}
@@ -75,10 +91,10 @@ foreach ($file in $src_files) {
 
 
 # Linking
-$obj_files = Get-ChildItem -Path Asm/
+$obj_files = Get-ChildItem -Path $intermediate_dir
 
 Write-Output "Linking"
-g++ $obj_files $C_FLAGS $LINKER_FLAGS -o $out_file -L $sdl_lib_dir
+g++ $obj_files $C_FLAGS $LINKER_FLAGS -L $sdl_lib_dir $sdl_linkables -o $out_file
 
 
 # Running current build
